fab7bfa93f6c57fa6624c877bfe55bd650a41ce8
Darren Maczka <dmaczka@vt.edu>
2013-09-23 12:17:02 -0400

    accept command line argument
    publish now uses the first command line argument, if present,
    as the deploy target. Defaults to 'public' if not supplied.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+target=${1:-public}
 
 git pull
 cd user-data
@@ -6,4 +8,4 @@ git pull
 cd -
 nanoc refresh_feeds <( cut -f 2 -d ' ' user-data/feeds )
 nanoc co
-nanoc deploy --target local
+nanoc deploy --target $target
