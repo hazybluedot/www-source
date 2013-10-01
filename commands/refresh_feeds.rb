14eca5475a1d88870343405c29e452968fcad97c
Darren Maczka <dmaczka@vt.edu>
2013-10-01 11:35:54 -0400

    clean out old fetch code
    no longer doing Atom parsing in refresh_feeds,
    just parallel pulls and saving unmodified feed
    data to file system

--- commands/refresh_feeds.rb
+++ commands/refresh_feeds.rb
@@ -2,7 +2,6 @@ usage 'refresh_feeds'
 aliases :refresh_feeds
 description 'Read list of feed URLs and update local links'
 
-require 'feedzirra'
 require 'uri'
 require 'stringex'
 require 'typhoeus'
@@ -14,34 +13,11 @@ class RefreshFeeds < Nanoc::CLI::Commands::CreateItem
     # Set VCS if possible
     self.set_vcs(options[:vcs])
 
-    Feedzirra::Feed.add_common_feed_entry_element('dc:creator', :as => :creator)
-
     arguments.each do |file_name|
       concurrent_fetch(file_name)
     end
   end
   
-  def old_fetch(file_name)
-    if File.readable?(file_name)
-      File.open(file_name).each do |uri|
-        feed = Feedzirra::Feed.fetch_and_parse(uri)
-        if feed
-          begin
-            puts feed.title
-            read_entries(feed)
-          rescue Exception => e
-            $stderr.puts 'Error: ' + uri
-            $stderr.puts e.message
-          end
-        else
-          $stderr.puts 'Error: ' + uri
-        end
-      end
-    else
-      $stderr.puts file_name+': Could not open file'
-    end
-  end
-
   def concurrent_fetch(file_name)
     hydra = Typhoeus::Hydra.new
     feeds = {}
@@ -49,67 +25,17 @@ class RefreshFeeds < Nanoc::CLI::Commands::CreateItem
       File.open(file_name).each do |uri|
         r = Typhoeus::Request.new(uri, followlocation: true)
         r.on_complete do |response|
-          begin
-            #feeds[r.url] = Feedzirra::Feed.parse(response.body)
-            feed_url = URI(r.url)
-            file_path = File.dirname(File.join(Dir.pwd, "feeds", feed_url.host.gsub('.','_'), feed_url.path))
-            Dir.exists?(file_path) ? nil : FileUtils.mkdir_p(file_path)
-            file_path = File.realpath(file_path)
-            file_path = File.join(file_path, 'feed.xml')
-            File.open(file_path, 'w+') { |file| file.write(response.body) }
-          rescue Feedzirra::NoParserAvailable => e
-            $stderr.puts 'Error: ' + uri
-          end
+          feed_url = URI(r.url)
+          feed_dir = File.dirname(File.join(Dir.pwd, "feeds", feed_url.host.gsub('.','_'), File.dirname(feed_url.path)))
+          Dir.exists?(feed_dir) ? nil : FileUtils.mkdir_p(feed_dir)
+          file_path = File.join(File.realpath(feed_dir), 'feed.xml')
+          File.open(file_path, 'w+') { |file| file.write(response.body) }
         end
         hydra.queue r
       end
       hydra.run
     else
       $stderr.puts file_name+': Could not open file'
-    end
-  end
-
-  def read_entries(feed)
-    syndicate_since = Time.parse(self.site.config[:syndicate_since] || 'Jan 01 00:00:01 GMT 1969')
-    
-    feed.entries.select { |e| (e.published > syndicate_since) && !((e.content || '') =~ /This is your first post\./) }.each do |entry|
-      #entry.sanitize!
-      entry_url = URI(entry.url)
-      host = entry_url.host
-      path_parts = entry_url.path.split("/")
-      entry.creator = path_parts[1] unless entry.creator
-      identifier = "/syndicated/" + host.gsub('.','_') + "/" + entry.creator + "/" + path_parts[2..-1].join("-").gsub('.','_') + "/"
-      exists = @site.items.find { |i| i.identifier == identifier }
-      entry.title = 'Untitled Post' unless entry.title
-      print "\t" + entry.title
-      if exists
-        exists[:title] = 'Untitled' unless exists[:title]
-        #puts "Item " + exists[:title] + " already exists... TODO: check for update"
-        print ' (exists... TODO: check for update)'
-      else
-        meta = {
-          :title => entry.title,
-          :entry_url => entry.url,
-          :kind => 'syndicate',
-          :created_at => entry.published,
-          :author_name => entry.author || entry.creator,
-          :author_uri => feed.url,
-          :tags => entry.categories
-        }
-
-
-        #create post if it doesn't exist
-        data_source = self.site.data_sources[0]
-        data_source.create_item(
-                                entry.content,
-                                meta,
-                                identifier,
-                                { :extension => '.html' }
-                                )
-        print ' (created)'
-        
-      end
-      print "\n"
     end
   end
 end
