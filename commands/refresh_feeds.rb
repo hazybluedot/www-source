d623a8de36597a26c77dc3061c5fc80d3dfa7461
Darren Maczka <dmaczka@vt.edu>
2013-10-11 16:31:46 -0400

    fix path used to save feed
    use of File.Dirname was not having the expected effect and
    all local paths were truncated

--- commands/refresh_feeds.rb
+++ commands/refresh_feeds.rb
@@ -6,12 +6,12 @@ require 'uri'
 require 'stringex'
 require 'typhoeus'
 
-class RefreshFeeds < Nanoc::CLI::Commands::CreateItem
+class RefreshFeeds < Nanoc::CLI::CommandRunner
   def run
-    self.require_site
+    #self.require_site
     
     # Set VCS if possible
-    self.set_vcs(options[:vcs])
+    #self.set_vcs(options[:vcs])
 
     arguments.each do |file_name|
       concurrent_fetch(file_name)
@@ -26,7 +26,7 @@ class RefreshFeeds < Nanoc::CLI::Commands::CreateItem
         r = Typhoeus::Request.new(uri, followlocation: true)
         r.on_complete do |response|
           feed_url = URI(r.url)
-          feed_dir = File.dirname(File.join(Dir.pwd, "feeds", feed_url.host.gsub('.','_'), File.dirname(feed_url.path)))
+          feed_dir = File.dirname(File.join(Dir.pwd, "feeds", feed_url.host.gsub('.','_'), feed_url.path))
           Dir.exists?(feed_dir) ? nil : FileUtils.mkdir_p(feed_dir)
           file_path = File.join(File.realpath(feed_dir), 'feed.xml')
           File.open(file_path, 'w+') { |file| file.write(response.body) }
